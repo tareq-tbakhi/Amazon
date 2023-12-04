a654f829a9520d0f153be447c771d096f63b9319
Tareq Tbakhi <tareq_tbakhi@hotmail.com>
2023-12-04 16:36:07 +0300

    10j

---
+++ exercises/10j.js
@@ -0,0 +1,13 @@
+let calculation = localStorage.getItem('calculation') || ''; 
+    document.querySelector('.js-calculation').innerHTML = calculation; 
+
+
+    function updateCalculation(value) {
+        calculation += value;
+        document.querySelector('.js-calculation').innerHTML = calculation; 
+        localStorage.setItem('calculation', calculation);
+      }
+    
+      function clearFunction(){
+        document.querySelector('.js-calculation').innerHTML = '';
+      }

f88d9a22f998cc411a9ea089249d44620ccb06c2
Tareq Tbakhi <tareq_tbakhi@hotmail.com>
2023-12-04 12:10:28 +0300

    10f

--- exercises/10f.js
+++ exercises/10f.js
@@ -1,8 +1,16 @@
 function checkButton(selector){
   const buttonElement = document.querySelector(selector);
   if(!buttonElement.classList.contains('is-toggled')){
+    turnOffOther();
     buttonElement.classList.add('is-toggled');
   }else{
     buttonElement.classList.remove('is-toggled');
+  }
+}
+
+function turnOffOther(){
+  const otherButton = document.querySelector('.is-toggled');
+  if(otherButton){
+    otherButton.classList.remove('is-toggled');
   }
 }

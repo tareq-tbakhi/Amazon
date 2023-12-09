0b590065390e2d622fc6ac4f8cfc83631a6973fc
Tareq Tbakhi <tareq_tbakhi@hotmail.com>
2023-12-09 20:22:50 +0300

    Todo-list

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,26 +1,32 @@
-const todoList = ['make dinner', 'wash dishes'];
+const todoList = [];
 
-let todoListHTML = '';
+renderTodoList();
 
-for(let i = 0; i < todoList.length; i++){
-  const todo = todoList[i];
-  const html = `<p>${todo}</p>`;
-  todoListHTML += html;
+function renderTodoList(){
+
+  let todoListHTML = '';
+
+  for(let i = 0; i < todoList.length; i++){
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`; // this is called generating the HTML 
+    todoListHTML += html;
+  }
+
+ // console.log(todoListHTML);
+
+  document.querySelector('.js-todo-list')
+  .innerHTML = todoListHTML;
 }
-
-console.log(todoListHTML);
-
-document.querySelector('.js-todo-list')
-.innerHTML = todoListHTML;
 
 
 
 function addTodo(){
-const inputElement = document.querySelector('.js-todo-input')
-const inputValue = inputElement.value
+  const inputElement = document.querySelector('.js-todo-input')
+  const inputValue = inputElement.value
 
-todoList.push(inputValue);
-console.log(todoList);
+  todoList.push(inputValue);
+  console.log(todoList);
 
-inputElement.value = '' ;
+  inputElement.value = '' ;
+
 }

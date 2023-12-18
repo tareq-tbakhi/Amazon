01bc01cf4967313e3905ca0b576b06bde664364d
Tareq Tbakhi <tareq_tbakhi@hotmail.com>
2023-12-18 11:50:39 +0300

    12-rock-paper-scissor

--- scripts/12-rock-paper-scissor.js
+++ scripts/12-rock-paper-scissor.js
@@ -17,6 +17,23 @@ if (!score) {
 }
 */
 
+let isAutoPlaying = false;
+let intervalId;
+
+function autoPlay(){
+  if(!isAutoPlaying){
+      intervalId = setInterval(function(){
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
+    isAutoPlaying = true;
+  } else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+  }
+}
+
+
 
 function playGame(playerMove) {
 const computerMove = pickComputerMove();
